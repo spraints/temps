776bcf38b053f6d1bb42161e938f56bacba49871
Matt Burke <spraints@gmail.com>
2020-04-13 17:29:31 -0400

    reformat css

--- pkg/static/appcss.go
+++ pkg/static/appcss.go
@@ -3,10 +3,19 @@ package static
 const AppCSS = "/app.css"
 
 const appCSS = `
-.temp-label { text-align: left; }
-.temp { text-align: right; }
-.temp-date { color: #ccc; }
-th, td { padding: 0.5em; }
+.temp-label {
+  text-align: left;
+}
+.temp {
+  text-align: right;
+}
+.temp-date {
+  color: #ccc;
+}
+th, td {
+  padding: 0.5em;
+}
+
 body {
   background-color: white;
   color: black;
