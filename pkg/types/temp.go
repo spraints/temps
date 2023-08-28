d6e6160d47830b5ceef3d1247bef3e9b0ba8b44d
Matt Burke <spraints@gmail.com>
2023-08-28 14:56:02 -0400

    Respect units when marshaling temperatures

--- pkg/types/temp.go
+++ pkg/types/temp.go
@@ -1,5 +1,7 @@
 package types
 
+import "encoding/json"
+
 type Temperature interface {
 	Celsius() Celsius
 	Fahrenheit() Fahrenheit
@@ -7,10 +9,30 @@ type Temperature interface {
 
 type Fahrenheit float64
 
+var _ json.Marshaler = Fahrenheit(0.0)
+
 func (f Fahrenheit) Celsius() Celsius       { return Celsius((f - 32.0) * 5.0 / 9.0) }
 func (f Fahrenheit) Fahrenheit() Fahrenheit { return f }
 
+func (f Fahrenheit) MarshalJSON() ([]byte, error) {
+	return tempAsJSON(f)
+}
+
 type Celsius float64
+
+var _ json.Marshaler = Celsius(0.0)
 
 func (c Celsius) Celsius() Celsius       { return c }
 func (c Celsius) Fahrenheit() Fahrenheit { return Fahrenheit(32.0 + c*9.0/5.0) }
+
+func (c Celsius) MarshalJSON() ([]byte, error) {
+	return tempAsJSON(c)
+}
+
+func tempAsJSON(t Temperature) ([]byte, error) {
+	type temp struct {
+		F float64 `json:"f"`
+		C float64 `json:"c"`
+	}
+	return json.Marshal(temp{F: float64(t.Fahrenheit()), C: float64(t.Celsius())})
+}
