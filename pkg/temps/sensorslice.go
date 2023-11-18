284096233f7782a67dd5b33378e0459f428e46ca
Matt Burke <spraints@gmail.com>
2023-11-17 20:48:07 -0500

    Sort by number
    It was bound to happen that I was going to end up with more than 10
    tags, and would want 11+ to be sorted after 1-9.

--- pkg/temps/sensorslice.go
+++ pkg/temps/sensorslice.go
@@ -17,9 +17,30 @@ func (s sensorSlice) Less(i, j int) bool {
 	if s[j].ID == outsideID {
 		return false
 	}
+
+	if a, ok := s.getIntFromName(i); ok {
+		if b, ok := s.getIntFromName(j); ok {
+			return a < b
+		}
+	}
+
 	return s[i].Name < s[j].Name
 }
 
 func (s sensorSlice) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
+}
+
+func (s sensorSlice) getIntFromName(i int) (n int, ok bool) {
+	str := s[i].Name
+	for j := 0; j < len(str); j++ {
+		c := str[j]
+		if c >= '0' && c <= '9' {
+			n = 10*n + int(c-'0')
+			ok = true
+		} else {
+			return
+		}
+	}
+	return
 }
--- pkg/temps/sensorslice_test.go
+++ pkg/temps/sensorslice_test.go
@@ -10,22 +10,74 @@ import (
 )
 
 func TestSortableSensors(t *testing.T) {
-	slice := sensorSlice(nil)
-	sort.Sort(slice)
-	assert.Equal(t, sensorSlice(nil), slice)
-
-	slice = sensorSlice([]types.Measurement{
-		{Name: "C", ID: "01"},
-		{Name: "A", ID: "zzyy"},
-		{Name: "B", ID: "ab"},
-		{Name: "Z", ID: outsideID},
+	t.Run("empty", func(t *testing.T) {
+		slice := sensorSlice(nil)
+		sort.Sort(slice)
+		assert.Equal(t, sensorSlice(nil), slice)
 	})
-	sort.Sort(slice)
-	// outsideID should be first, other IDs shouldn't matter.
-	assert.Equal(t, sensorSlice([]types.Measurement{
-		{Name: "Z", ID: outsideID},
-		{Name: "A", ID: "zzyy"},
-		{Name: "B", ID: "ab"},
-		{Name: "C", ID: "01"},
-	}), slice)
+
+	t.Run("sort by name", func(t *testing.T) {
+		slice := sensorSlice([]types.Measurement{
+			{Name: "C", ID: "01"},
+			{Name: "A", ID: "zzyy"},
+			{Name: "B", ID: "ab"},
+			{Name: "Z", ID: outsideID},
+		})
+		sort.Sort(slice)
+		// outsideID should be first, other IDs shouldn't matter.
+		assert.Equal(t, sensorSlice([]types.Measurement{
+			{Name: "Z", ID: outsideID},
+			{Name: "A", ID: "zzyy"},
+			{Name: "B", ID: "ab"},
+			{Name: "C", ID: "01"},
+		}), slice)
+	})
+
+	t.Run("with numbers in names", func(t *testing.T) {
+		slice := sensorSlice([]types.Measurement{
+			{Name: "1 C", ID: "01"},
+			{Name: "10 A", ID: "zzyy"},
+			{Name: "2 B", ID: "ab"},
+			{Name: "Z", ID: outsideID},
+		})
+		sort.Sort(slice)
+		// outsideID should still be first, other IDs should be sorted numerically.
+		assert.Equal(t, sensorSlice([]types.Measurement{
+			{Name: "Z", ID: outsideID},
+			{Name: "1 C", ID: "01"},
+			{Name: "2 B", ID: "ab"},
+			{Name: "10 A", ID: "zzyy"},
+		}), slice)
+	})
+}
+
+func TestGetIntFromName(t *testing.T) {
+	shouldWork := []struct {
+		name     string
+		expected int
+	}{
+		{"1", 1},
+		{"11", 11},
+		{"1 hi", 1},
+		{"99999 @#$%^&*", 99999},
+	}
+
+	shouldNotWork := []string{
+		"ok1",
+		"no",
+	}
+
+	for _, ex := range shouldWork {
+		slice := sensorSlice([]types.Measurement{{Name: ex.name}})
+		n, ok := slice.getIntFromName(0)
+		if assert.Truef(t, ok, "should find number in %q", ex.name) {
+			assert.Equalf(t, ex.expected, n, "number in %q", ex.name)
+		}
+	}
+
+	for _, ex := range shouldNotWork {
+		slice := sensorSlice([]types.Measurement{{Name: ex}})
+		n, ok := slice.getIntFromName(0)
+		assert.Falsef(t, ok, "should not find number in %q but found %d", ex, n)
+	}
 }
